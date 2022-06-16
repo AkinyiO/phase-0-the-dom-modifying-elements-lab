6b828a9d4ba103ae91a71fba268f2b506b8407b3
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-16 21:24:22 +0300

    commit tree 60b822acda8c3aff270e1399d7034327116829ec

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const testVar = "Hello"
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Akinyi is the champion!";
+newHeader.className = "victory";
